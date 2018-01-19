47d97c0f6aff21b830f0d47827e1b5efa2780fa8
jblau07 <jlau7248@gmail.com>
2018-01-18 14:20:14 -1000

    mobile view looks good

--- public/js/app.js
+++ public/js/app.js
@@ -5,11 +5,11 @@ const finance = "https://www.reddit.com/r/personalfinance.json";
 
 const metal = "https://www.reddit.com/r/natureismetal.json";
 
-
-
 let random = document.createElement("a");
 random.href = "#";
-random.innerHTML = "Random"
+random.className = "button";
+random.id = "random";
+random.innerHTML = "RANDOM"
 document.getElementById("topNav").appendChild(random);
 // random.addEventListener("click", function(){
 //   getReddit("https://www.reddit.com/r/aww/.json");
@@ -17,7 +17,9 @@ document.getElementById("topNav").appendChild(random);
 
 let myBoards = document.createElement("a");
 myBoards.href = "#";
-myBoards.innerHTML = "My Boards";
+myBoards.innerHTML = "MY BOARDS";
+myBoards.className = "button";
+myBoards.id = "boards";
 document.getElementById("topNav").appendChild(myBoards);
 myBoards.addEventListener("click", function () {
   getReddit(finance);
@@ -25,7 +27,9 @@ myBoards.addEventListener("click", function () {
 
 let getApp = document.createElement("a");
 getApp.href = "#";
-getApp.innerHTML = "Get the App"
+getApp.innerHTML = "GET THE APP";
+getApp.className = "button";
+getApp.id = "app"
 document.getElementById("topNav").appendChild(getApp);
 getApp.addEventListener("click", function () {
   getReddit(metal);
@@ -53,7 +57,7 @@ function fillIn(redditInfo, parentElem) {
     // tile.className = "tilez";
     let tile = document.createElement("div");
     tile.className = "tilez";
-    console.log(element.data.created_utc);
+    //console.log(element.data.created_utc);
 
     //const previewPic = element.data.preview;
     let postPic = document.createElement("img");
@@ -88,7 +92,7 @@ function fillIn(redditInfo, parentElem) {
 
     let postAuthor = document.createElement("div");
     postAuthor.className = "awwAuthor";
-    postAuthor.innerHTML = "by " + element.data.author + " created " + moment.unix(element.data.created_utc).format('M-DD-YY');
+    postAuthor.innerHTML = "by " + element.data.author + "  • created " + moment.unix(element.data.created_utc).format('M-DD-YY') + " • Score " + element.data.score;
     tile.appendChild(postAuthor);
 
     // let postDate = document.createElement("div");
@@ -96,20 +100,21 @@ function fillIn(redditInfo, parentElem) {
     // postDate.innerHTML = "Created on " + moment.unix(element.data.created_utc).format('M-DD-YY');
     // tile.appendChild(postDate);
 
-    let postScore = document.createElement("div");
-    postScore.className = "awwScore";
-    postScore.innerHTML = "Score " + element.data.score;
-    tile.appendChild(postScore);
+    // let postScore = document.createElement("div");
+    // postScore.className = "awwScore";
+    // postScore.innerHTML = " • Score " + element.data.score;
+    // tile.appendChild(postScore);
 
     let postText = document.createElement("div");
     postText.className = "awwText";
     if (element.data.selftext !== "") {
-      postText.innerHTML = element.data.selftext
+      postText.innerHTML = element.data.selftext;
     } else {
       postText.innerHTML = "No inner text."
     }
     tile.appendChild(postText);
     collection.appendChild(tile);
+
   })
   parentElem.innerHTML = "";
   parentElem.appendChild(collection);
@@ -117,19 +122,6 @@ function fillIn(redditInfo, parentElem) {
 }
 
 getReddit("https://www.reddit.com/r/aww/.json");
-// getReddit("https://www.reddit.com/r/personalfinance.json")
-
-
-
-
-// let postDate = document.createElement("div");
-// postDate.className = "awwDate";
-//  postDate.innerHTML = moment.unix(element.data.create).format('MM/DD/YYYY');
-//  postDate.appendChild(postScore);
-
-
-// let postPic = document.createElement("img");
-// postPic.className = "awwPic";
 
 // getMain.appendChild(postTitle)
 
@@ -151,66 +143,3 @@ getReddit("https://www.reddit.com/r/aww/.json");
 
 
 // }
-
-
-// document.getElementById("mainContainer").innerHTML = fillIn();
-
-
-// else{
-//   postPic.setAttribute("src", "https://www.blog.google/static/blog/images/google-200x200.7714256da16f.png");
-//   // postPic.setAttribute("src", element.data.url);
-//   postPic.className = "awwPic";
-//   postTitle.appendChild(postPic);
-
-//   postTitle.appendChild(postPic);
-//   postPic.className = "awwPic";
-//   postTitle.appendChild(postPic);
-// if (element.data.preview.images[0].source){
-//   postPic.setAttribute("src", element.data.preview.images[0].source.url);
-//   postPic.className = "awwPic";
-//   postTitle.appendChild(postPic);
-// }else {
-//   postPic.setAttribute("src", "https://www.pexels.com/photo/man-wearing-gray-crew-neck-shirt-holding-basketball-804121/");
-//   postPic.className = "awwPic";
-//   postTitle.appendChild(postPic);
-
-
-
-
-
-// postPic.setAttribute("src", element.data.thumbnail)
-// postPic.className = "awwPic";
-// // postTitle.appendChild(postPic);
-// let postComments = document.createElement("div");
-// postComments.className = "awwComments";
-// postComments.innerHTML = element.data.num_comments + " comments";
-// postTitle.appendChild(postComments);
-
-
-// let awwReq = new XMLHttpRequest(); 
-// awwReq.addEventListener("load", fillIn); 
-// awwReq.open("GET", "https://www.reddit.com/r/aww.json"); 
-// awwReq.send();
-
-
-
-// let titles = JSON.parse(this.response);
-//   let getMain = document.getElementById("mainContainer");
-//   let postTitle = document.createElement("div");
-
-//   postTitle.className = "nbaPost";
-//   postTitle.innerHTML = titles.data.children[0].data.title
-//   getMain.appendChild(postTitle);
-//   console.log(titles.data.children[0].data)
-//   let postAuthor = document.createElement("div");
-//   postAuthor.className = "nbaAuthor";
-//   postAuthor.innerHTML = titles.data.children[0].data.author;
-//   postTitle.appendChild(postAuthor);
-//   let postPic = document.createElement("img");
-//   postPic.setAttribute("src", titles.data.children[0].data.thumbnail)
-//   postPic.className = "nbaPic";
-//   postTitle.appendChild(postPic);
-//   let postComments = document.createElement("div");
-//   postComments.className = "nbaComments";
-//   postComments.innerHTML = titles.data.children[0].data.num_comments + " comments";
-//   postTitle.appendChild(postComments)
